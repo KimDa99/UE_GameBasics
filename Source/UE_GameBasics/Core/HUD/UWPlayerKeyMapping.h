2e03b384406e1c2d5c9ab57c3d3350ab9bdb3d92
KimDa99 <hyhy61451@gmail.com>
2025-02-11 11:57:43 +0900

    add: add User Widget BluePrint and Class for Player Key Mapping

---
+++ Source/UE_GameBasics/Core/HUD/UWPlayerKeyMapping.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "UWPlayerKeyMapping.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UE_GAMEBASICS_API UUWPlayerKeyMapping : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
